c38ad991f7757b698d9b9defa82f28afa47cb24b
RAFAEL DE OLIVEIRA PEIXOTO MEDEIROS <a92328933@rn.senac.local>
2024-09-09 21:19:20 -0300

    Pasta JS Adicionada

---
+++ JS/Aula01.js
@@ -0,0 +1,38 @@
+console.log('olá mundo')
+
+let nome = 'Rafael';
+
+console.log(nome)
+
+console.log(typeof(nome))
+
+// let x = 8;
+// console.log(x)
+
+// console.log(typeof(x))
+
+// alert('Instalde Node')
+
+// let palavra = prompt("Digite seu nome:")
+
+// alert(palavra)
+
+// let x = parseFloat(prompt("Digite um número: "));
+
+// let y = parseFloat(prompt('Digite outro número: '));
+
+// let a = x + y
+
+// alert(a)
+
+
+//  let x = parseFloat(prompt("Digite um número: "));
+
+//  let y = parseFloat(prompt('Digite outro número: '));
+
+//  let a = x % y
+
+//  alert(a)
+
+
+ console.log(x)
