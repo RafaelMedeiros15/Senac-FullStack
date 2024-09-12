97e96daa1b49db7c57a963c55abb6a2ad267fe9e
RAFAEL DE OLIVEIRA PEIXOTO MEDEIROS <a92328933@rn.senac.local>
2024-09-11 21:29:07 -0300

    operadores aritmeticos

---
+++ JS/Atividade02.js
@@ -0,0 +1,20 @@
+let n1 = Number(prompt('Digite um número'))
+let n2 = Number(prompt('Digite outro número'))
+
+let soma = n1 + n2;
+let subtraçao = n1 - n2;
+let mult = n1 * n2;
+let divisao = n1 / n2;
+let modulo = n1 % n2;
+let potencia = n1 ** n2;
+let raiz = n1**0.5
+
+
+
+console.log(`A soma é: ${soma}`)
+console.log(`A subtração é: ${subtraçao}`)
+console.log(`A multiplicação é: ${mult}`)
+console.log(`A divisão é: ${divisao}`)
+console.log(`A modulo é: ${modulo}`)
+console.log(`A potencia é: ${modulo}`)
+console.log(`A raiz quadrada é: ${raiz}`)
---
+++ JS/Aula03.js
@@ -0,0 +1,63 @@
+// Operadores Matemáticos
+
+// let n1 = 4;
+// let n2 = 2;
+
+// let soma = n1 + n2;
+// let mult = n1 * n2;
+// let divisao = n1 / n2;
+// let modulo = n1 % n2;
+// let potencia = n1**n2;
+
+// console.log(n1)
+// console.log(n2)
+
+// console.log(n1 + n2);
+// console.log(soma)
+// console.log(mult)
+// console.log(divisao)
+// console.log(modulo)
+// console.log(potencia)
+
+console.clear()
+
+// console.log(n1++)
+// console.log(n1)
+
+// console.log(++n2)
+
+
+// console.log(n1--)
+// console.log(n1)
+
+// console.log(--n2)
+
+
+
+// n1 += 2;
+// console.log(n1)
+
+// let x = 'Douglas '
+// let y = 'Lemos'
+
+// console.log(x + y)
+
+
+
+                // Operadores Lógicos
+            
+
+// && -> E!
+// || -> Ou!
+// ! -> Não!
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+
+// const a = false
+// const b = true
+// const c = false
+// const d = true
