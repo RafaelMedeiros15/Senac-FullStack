0d376e8ce1f955a9b35831c19b69c5abf9641053
RAFAEL DE OLIVEIRA PEIXOTO MEDEIROS <a92328933@rn.senac.local>
2024-09-20 21:10:45 -0300

    Operadores Ternários

---
+++ JS/Aula09-Operador-Ternario.js
@@ -0,0 +1,67 @@
+/*let n = Number(prompt('Digite um número: '));*/
+
+/*if(n % 2 === 0){
+    alert(true)
+}else{
+    alert(false)
+}*/
+
+//#########################################################
+
+/*let resultado = (n % 2 === 0) ? "Par" : "Impar"
+
+alert(`Você digitou o número ${n} e o resultado é ${resultado}`)*/
+
+ //##############################################################################################
+
+/*
+let resultado = (n >= 0) ? "POSITIVO" : "NEGATIVO";
+
+alert(`Você colocou um número ${resultado}`);*/
+
+
+//####################################################################
+
+/* Verificar se é maior ou menor de idade;
+
+let idade = Number(prompt('Informa sua idade: '))
+
+let resultado = (idade >= 18 ) ? "Maior de idade!" : "Menor de idade!"
+
+alert(`Você tem ${idade} e é ${resultado}`)*/
+
+
+//####################################################################
+
+/*let n = Number(prompt('Digite um número: '))
+
+let intervalo = (n >= 10 && n <= 20) ? "SIM!" : "Não!"
+
+alert(`Seu número está entre 10 e 20? ${intervalo} Você digitou o numero ${n}!`);*/
+
+//####################################################################
+
+/*let nota = Number(prompt('Digite sua nota: '))
+
+let media = (nota >= 60) ? alert("Você Passou com a nota: " +`${nota}`) : alert("Você não passou por ter tirado a nota: " + `${nota}`)*/
+
+
+//####################################################################
+
+/*let nota = Number(prompt('Informe sua nota: '))
+
+let classe = (nota >= 90) ? alert("Você tirou nota A!"):
+             (nota >= 80) ? alert("Você tirou nota B!"):
+             (nota >= 70) ? alert("Você tirou nota C!"):
+             (nota >= 60) ? alert("Você tirou nota D!"):
+             alert("Você tirou nota F!");*/
+             
+
+//####################################################################
+
+
+/*let n = Number(prompt('Digite um número'))
+
+let resultado = (n % 3 === 0 && n % 5 === 0) ? "É divisivel por 3 e 5 Sim" : "Você digitou um número que NÃO é divisivel por 3 e 5"
+
+alert(`${resultado}. Você digitou o número: ${n}`)*/
